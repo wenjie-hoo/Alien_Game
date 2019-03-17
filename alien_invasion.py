5e871947144a7db05fd29669e5062223b71197e5
Clark Hu <clark0712@foxmail.com>
2019-03-17 14:28:06 +0800

    创建了一个空的pygame窗口

---
+++ alien_invasion.py
@@ -0,0 +1,25 @@
+import sys
+import pygame
+
+def run_game():
+    #   初始化游戏并创造一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Alien Invasion")
+    #   创建背景颜色
+    bg_color = (230,230,230)
+
+    #   开始游戏的主循环
+    while True:
+        #   监听鼠标和键盘事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        #   每次循环时都刷新
+        screen.fill(bg_color)
+
+        #   让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
