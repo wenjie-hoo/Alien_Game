c3d357f42677f21378fc9081ede26bd91ff240bf
Clark Hu <clark0712@foxmail.com>
2019-03-21 15:35:01 +0800

    控制飞船不飞出屏幕外界

--- ship.py
+++ ship.py
@@ -27,9 +27,9 @@ class Ship():
 
         '''根据移动标志调整飞船位置'''
         #   更新飞船的centerx值，而不是rect
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.center -=self.ai_settings.ship_speed_factor
 
         #   根据self.center更新rect对象
