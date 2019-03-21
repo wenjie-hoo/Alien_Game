a8f20e4414a2bb0d79f4096c49451a1e37312943
Clark Hu <clark0712@foxmail.com>
2019-03-21 15:52:01 +0800

    重构键盘响应事件

--- game_functions.py
+++ game_functions.py
@@ -1,21 +1,29 @@
 import sys
 import pygame
 
+def check_keydown_event(event,ship):
+    '''按键响应'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_event(event,ship):
+    '''响应松开'''
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
+    '''响应键盘事件'''
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-                #向右移动飞船
+            check_keydown_event(event,ship)
         elif event.type ==pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFTL:
-                ship.moving_left = False
+            check_keyup_event(event,ship)
 
 def update_screen(ai_settings,screen,ship):
     #   每次循环时都刷新
