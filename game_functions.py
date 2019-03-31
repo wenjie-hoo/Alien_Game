613d7750d8ca9fb91540dd5f88127c38eb6f2108
Clark Hu <clark0712@foxmail.com>
2019-03-31 21:48:28 +0800

    检测子弹与外星人碰撞

--- game_functions.py
+++ game_functions.py
@@ -47,12 +47,16 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
     #   让最近绘制的屏幕可见
     pygame.display.flip()
 
-def update_bullets(bullets):
+def update_bullets(aliens,bullets):
     # 删除已消失的子弹
     bullets.update()
     for bullet in bullets.copy():
         if bullet.rect.bottom < 0:
             bullets.remove(bullet)
+    '''更新子弹位置，并删除已经消失的子弹'''
+    #   检查是否有子弹击中了外星人
+    #   如果是这样，就删除相应的子弹和外星人
+    collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
 def update_alien(aliens):
     '''更新外星人群中所有的外星人位置'''
