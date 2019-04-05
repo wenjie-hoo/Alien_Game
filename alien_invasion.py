b4d47d6055bf54d1834a6de9d8adf695e05e4aa5
Clark Hu <clark0712@foxmail.com>
2019-04-05 21:54:03 +0800

    i don't kown what i code today

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from ship import Ship
 from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats
 
 def run_game():
     #   初始化游戏并创造一个屏幕对象
@@ -13,12 +14,16 @@ def run_game():
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    #   创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+
     #   创建一个飞船
     ship = Ship(ai_settings,screen)
 
     #   创建一个用于储存子弹的编组
     bullets = Group()
     aliens = Group()
+
     #   创建外星群
     gf.create_fleet(ai_settings,screen,ship,aliens)
 
@@ -26,9 +31,11 @@ def run_game():
     while True:
         #   监听鼠标和键盘事件
         gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_aliens(ai_settings,aliens)
-        gf.update_bullets(bullets,aliens)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -4,17 +4,20 @@ class Setting():
         self.screen_width = 900
         self.screen_height = 600
         self.bg_color = (230,230,230)
-        self.ship_speed_factor = 3
 
         #   外星人设置
-        self.alien_speed_factor = 1
-        self.fleet_drop_speed = 10
+        self.alien_speed_factor = 10
+        self.fleet_drop_speed = 30
         #   fleed_direction为1表示向右边移动，-1代表向左边移动
         self.fleet_direction = 1
 
         #   子弹设置
-        self.bullet_speed_factor = 2
-        self.bullet_width = 3
+        self.bullet_speed_factor = 5
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullet_allowed = 3
+
+        #   飞船设置
+        self.ship_limit = 3
+        self.ship_speed_factor = 1.5
