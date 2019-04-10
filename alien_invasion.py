094db4b9638dc129cb84fdca0a935c7f0b11b629
Clark Hu <clark0712@foxmail.com>
2019-04-10 23:34:36 +0800

    添加"开始"按钮

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 from game_stats import GameStats
+from buttom import Button
 
 def run_game():
     #   初始化游戏并创造一个屏幕对象
@@ -16,6 +17,9 @@ def run_game():
 
     #   创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
+
+    #   创建一个play按钮
+    play_button = Button(ai_settings,screen,"Play")
 
     #   创建一个飞船
     ship = Ship(ai_settings,screen)
@@ -30,12 +34,12 @@ def run_game():
     #   开始游戏的主循环
     while True:
         #   监听鼠标和键盘事件
-        gf.check_events(ai_settings,screen,ship,bullets)
+        gf.check_events(ai_settings,screen,stats,play_button,ship,bullets)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
             gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
-        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+        gf.update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button)
 
 run_game()
